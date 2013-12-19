02907b3508bd92a624ed7850afa4469422e4dd04
zhang-ning <hunter.dding@gmail.com>
2013-12-19 22:24:53 +0800

    implement jchangeSpec

--- test/grammer/jchangeSpec.js
+++ test/grammer/jchangeSpec.js
@@ -3,32 +3,46 @@ main(function(){
   var $scan = this.need('river.engine').scan;
   var trigger = this.need('river.scenario').trigger;
 
-
   describe("The jChange grammer feature.",function(){
-
-
     var foo;
-
     beforeEach(function(){
       foo = {
         changeCtrl : function(){
         }
       };
-    });
+      spyOn(foo,'changeCtrl');
 
-    it("when the select input element options changed,the fn should be called.",function(){
       /**
        * the spec module is for testing.
        **/
       define('spec.jchange',function(){
         return function(){
-          this.ChangeCtrl = function(val){
-          };
+          this.changeCtrl = foo.changeCtrl;
         };
       });
-      var element = $compile('<select scope="spec.jchange" jchange="changeCtrl"><option value="f">f</option><option value="s">s</option></select>');
-      //$scan(element);
-      //expect('jbind works').toBe(element.textContent);
+      var element = $compile('<select scope="spec.jchange" jchange="changeCtrl"><option value="f">first</option><option value="s">second</option></select>');
+
+      $scan(element);
+      element.value = "f";
+      trigger('change',element);
+      element.value = "s";
+      trigger('change',element);
+    });
+
+    it("when the select input element options changed,the fn should be called.",function(){
+      expect(foo.changeCtrl).toHaveBeenCalled();
+    });
+
+    it("have been called twice",function(){
+      expect(foo.changeCtrl.calls.length).toEqual(2);
+    });
+
+    it("first call parameters should be f",function(){
+      expect(foo.changeCtrl.calls[0].args[0]).toEqual("f");
+    });
+    
+    it("second call parameters should be s",function(){
+      expect(foo.changeCtrl.calls[1].args[0]).toEqual("s");
     });
   }); 
 });
