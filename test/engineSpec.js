db948d7a540e8ff2da200643845ff29d0573af38
zhang-ning <hunter.dding@gmail.com>
2013-12-24 20:51:21 +0800

    fix bug on end of line symble

--- test/engineSpec.js
+++ test/engineSpec.js
@@ -1,22 +1,44 @@
 describe('The template enginee render feature ',function(){
 
-  var $compile;
+  var $compile,$scan;
 
-  main(function(){
-    $compile = this.need('river.core.tools').compile;
-  });
-
-  it("test scope work",function(){
+  beforeEach(function(){
+    main(function(){
+      $compile = this.need('river.core.tools').compile;
+      $scan = this.need('river.engine').scan;
+    });
     define('testCtrl',function(){
       return { message : "hello scope" };
     });
-
-    var element = $compile("<div scope='testCtrl'>{{ message }}</div>");
-
-    main(function(){
-      this.need('river.engine').scan(element);
-      expect("hello scope").toBe(element.textContent);
-    });
   });
 
+  it("test scope work",function(){
+    var element = $compile("<div scope='testCtrl'>{{ message }}</div>");
+    $scan(element);
+    expect("hello scope").toBe(element.textContent);
+  });
+
+  it("the \\r out of expression and in tag",function(){
+    var element = $compile("<div scope='testCtrl'>{{ message }}\r</div>");
+    $scan(element);
+    expect("hello scope").toBe(element.textContent);
+  });
+
+  it("the \\r in expression",function(){
+    var element = $compile("<div scope='testCtrl'>{{ message \r}}</div>");
+    $scan(element);
+    expect("hello scope").toBe(element.textContent);
+  });
+
+  it("the \\n out of expression and in tag",function(){
+    var element = $compile("<div scope='testCtrl'>{{ message }}\n</div>");
+    $scan(element);
+    expect("hello scope").toBe(element.textContent);
+  });
+
+  it("the \\n in expression",function(){
+    var element = $compile("<div scope='testCtrl'>{{ message \n}}</div>");
+    $scan(element);
+    expect("hello scope").toBe(element.textContent);
+  });
 });
