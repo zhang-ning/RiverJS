6acad8c25d9238b8c39f18deef6d8ed11992b21e
zhang-ning <hunter.dding@gmail.com>
2014-01-08 20:15:23 +0800

    notify

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,6 +38,13 @@ module.exports = function(grunt) {
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint', 'qunit']
+    },
+    notify:{
+      build:{
+        options:{
+          message: 'build finished'
+        }
+      }
     }
   });
 
@@ -46,8 +53,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-notify');
 
   grunt.registerTask('test', ['jshint', 'qunit']);
 
-  grunt.registerTask('default', ['jshint', 'concat' ]);
+  grunt.registerTask('default', ['jshint', 'concat','notify']);
 };
