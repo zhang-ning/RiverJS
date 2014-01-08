168e4309f9bff6a3e1ad06172f068571c06b59b9
zhang-ning <hunter.dding@gmail.com>
2014-01-08 16:49:05 +0800

    change

--- dist/river.js
+++ dist/river.js
@@ -429,6 +429,15 @@ define('river.grammer.jclick', function() {
 });
 define('river.grammer.jcompile',function(){
   return function(){
+    //jcompile should never be used when sub tag structutor contain any other grammer tag,cause it will be totally replace by innnerHTML.
+
+    var element = this.node;
+    var scope = this.scope;
+    var reg = this.reg;
+
+    var key = element.textContent.replace(reg,'');
+    //element.innerHTML = scope[key];
+//    console.log(this.eom.msg);
   };
 });
 define("river.grammer.repeat", function() {
