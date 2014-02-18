bdab05056f62bb192329024ea79d8e314c5d97b5
zhang-ning <hunter.dding@gmail.com>
2014-02-18 23:26:02 +0800

    pass data

--- test/grammer/jonSpec.js
+++ test/grammer/jonSpec.js
@@ -6,10 +6,11 @@ main(function(){
   var $key = scenario.key;
 
   describe("test jon grammer feature.",function(){
-    var foo,me;
+    var foo,me,input,msg;
     beforeEach(function(){
       foo = {
         ctrl : function(event,data){
+          foo.data = data;
           if(event.keyCode == 13){
             me.msg = 'hello world';
           }
@@ -26,31 +27,46 @@ main(function(){
       });
 
       var dom = $compile('<div scope="spec.jon">' +
-          '<input type="text" jon="keydown | ctrl"/>' +
-          '<p>{{ msg }}</p>' +
-          '</div>');
+                         '<input type="text" jon="keydown | ctrl(msg)" jbind="msg"/>' +
+                         '<p>{{ msg }}</p>' +
+                         '</div>');
       $scan(dom);
-      var input = dom.querySelector("input");
-      var msg = dom.querySelector("p");
+      input = dom.querySelector("input");
+      msg = dom.querySelector("p");
+    });
 
+    it("test jbind",function(){
+      $trigger('focus',input);
+      input.value = 'good night';
+      var flag = false;
+      runs(function(){
+        setTimeout(function(){
+          $trigger('blur',input);
+          flag = true;
+        },100);
+      });
+      waitsFor(function(){ 
+        return flag;
+      },'somethings',100);
+      runs(function(){
+        expect('good night').toBe(msg.textContent);
+        $key('keydown',13,'enter',input);
+      });
+    });
+
+
+    it("first result should be hello",function(){
       $key('keydown',14,'enter',input);
       foo.result2 = msg.textContent;
+      expect(foo.result2).toEqual('undefined');
+      expect(foo.ctrl.calls.length).toEqual(1);
+    });
 
+    it("first result should be hello",function(){
       $key('keydown',13,'enter',input);
       foo.result1 = msg.textContent;
-
-    });
-
-    it("clickCtrl should be called twice",function(){
-      expect(foo.ctrl.calls.length).toEqual(2);
-    });
-
-    it("first result should be hello",function(){
-      expect(foo.result2).toEqual('undefined');
-    });
-
-    it("first result should be hello",function(){
       expect(foo.result1).toEqual("hello world");
+      expect(foo.ctrl.calls.length).toEqual(1);
     });
 
   });
