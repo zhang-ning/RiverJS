3582030c4d740535c3d9db7e22f684e66633c054
zhang-ning <hunter.dding@gmail.com>
2014-02-20 11:50:02 +0800

    add new test case on exports,module.exports,require fix on bug

--- test/cmdSpec.js
+++ test/cmdSpec.js
@@ -18,6 +18,22 @@ describe("The CMD module define feature", function() {
     return this.need('test.B'); 
   });
 
+  define('test.D',function(exports,require,module){
+    var c = require('test.C');
+    exports.name='jonathan';
+    exports.getName = function(){
+      return this.name;
+    }
+    exports.c = c;
+  });
+
+  define('test.E',function(exports,require,module){
+    var D = require('test.D');
+    exports = module.exports = {
+      msg : D.name
+    }
+  });
+
   it("test.B can call test.A", function() {
     main(function(){
       expect('mk').toBe(this.need('test.B'));
@@ -27,6 +43,24 @@ describe("The CMD module define feature", function() {
   it("the nested dependence", function() {
     main(function(){
       expect('mk').toBe(this.need('test.C'));
+    });
+  });
+
+  it("test.D : exports case", function() {
+    main(function(exports,require,module){
+      expect('jonathan').toBe(require('test.D').name);
+    });
+  });
+
+  it("test.D: nest dependence", function() {
+    main(function(exports,require,module){
+      expect('mk').toBe(require('test.D').c);
+    });
+  });
+
+  it("test.E : module.exports", function() {
+    main(function(exports,require,module){
+      expect('jonathan').toBe(require('test.E').msg);
     });
   });
 
