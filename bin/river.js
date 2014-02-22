aeed56577a3c0bfa8c9fbc9ba59e591c50455b98
zhang-ning <hunter.dding@gmail.com>
2014-02-22 16:35:21 +0800

    add riverjs-cli

--- bin/river.js
+++ bin/river.js
@@ -1,27 +1,111 @@
 #!/usr/bin/env node
 
 var command = {}
-  , fs = require('fs');
+  , fs = require('fs')
+  , exclude = /node_modules/
+  , targetfile = /\.js\s*$/
+  , rootPath;
 
 command.help = function(){
   console.log([
     "",
     "welcome to use riverjs build tool",
     "How to use ?",
-    "riverjs [project path]",
+    "Build ------------------ riverjs build [project path]",
+    "Help ------------------- riverjs help",
+    "Clean ------------------ riverjs clean [project path]",
     ""
   ].join('\n'));
 };
 
+command.clean = function () {
+  clean(process.argv[3] + '/build');
+}
+
+command.build = function (){
+  begin(process.argv[3]);
+}
+var bin = command[process.argv[2]]
+if(typeof bin === 'function'){
+  bin.call(null);
+}else{
+  command.help();
+}
+
+
+
 function begin(path) {
   if(!path) return command.help();
+  path = path.replace(/\/\s*$/,'');
+  rootPath = path;
+  readPath(path);
+  clean(path + '/build');
+  buildFile(path+ '/build');
+}
 
-  fs.readdir(path,function(err,value){
-    console.log(value);
-  });
-  fs.ReadStream(path,function(err,value){
-    console.log(value);
+function readPath(path){
+  fs.stat(path,function(err,value){
+    if(err) throw err;
+    if(value.isFile()) readFile(path);
+    if(value.isDirectory()) readDirectory(path);
   });
 }
 
-begin(process.argv[2]);
+function readFile(path){
+  if(!targetfile.test(path)) return;
+  fs.readFile(path,'utf8',function(err,value){
+    var namespace = path.replace(/^\.\/|\.js$/g,'').replace(/\//g,'.');
+    var data = header(namespace)  + '\n' + value + '\n' + footer() + '\n';
+    appendToBuffer(data);
+  });
+}
+
+function readDirectory(path){
+  if(exclude.test(path)) return;
+  fs.readdir(path,function(err,value){
+    if(err) throw err;
+    for (var i = 0, len = value.length; i < len; i++) {
+      var childpath = path + '/' + value[i];
+      readPath(childpath);
+    }
+  });
+}
+
+function clean(path){
+  var stat; 
+  try{
+    stat = fs.statSync(path);
+  }catch(err){
+    return;
+  }
+  if(!stat) return;
+  if(stat.isDirectory()){
+    var subpath = fs.readdirSync(path);
+    for (var i = 0, len = subpath.length; i < len; i++) {
+      clean(path + '/' + subpath[i]);
+    }
+    fs.rmdir(path);
+  }else if(stat.isFile()){
+    fs.unlinkSync(path);
+  }
+}
+
+function buildFile(path) {
+  fs.mkdir(path)
+}
+
+function header(path) {
+  return 'define("'+ path +'",function(exports,require,module){';
+}
+
+function footer() {
+  return '});';
+}
+
+
+function appendToBuffer(data) {
+  fs.appendFile(rootPath + '/build/app.js',data,function(err){
+    if(err) throw err;
+  });
+}
+
