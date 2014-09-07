062e938ac1be18ebca3e43e0247e3fead56a44a0
zhang-ning <hunter.dding@gmail.com>
2014-09-07 10:34:40 +0800

    test fugitive

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 var fs = require("fs"),
     path = require("path");
-module.exports = new Function("return" +
-  fs.readFileSync(path.join(__dirname,"dist/river.js"),"utf-8"));
+module.exports = new Function("return" + fs.readFileSync(path.join(__dirname,"dist/river.js"),"utf-8"));
